1c66e832aacef07a2761d32b64919f3ce824ad66
Gordon Mackie <gordon.mackie@openfeature.co.uk>
2020-10-14 14:13:50 +0100

    Basic Watch components added

---
+++ PracticeRecord.WatchOS/PracticeRecord.WatchOS.WatchOSExtension/ExtensionDelegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using Foundation;
+using WatchKit;
+
+namespace PracticeRecord.WatchOS.WatchOSExtension
+{
+    public class ExtensionDelegate : WKExtensionDelegate
+    {
+        public ExtensionDelegate()
+        {
+        }
+    }
+}
+
